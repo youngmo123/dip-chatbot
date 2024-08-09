a1f2d00aee737368fdcc4a5229d50da3f2a19a5c
youngmo123 <46598482+youngmo123@users.noreply.github.com>
2024-08-09 15:44:57 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import streamlit as st
 import os
 
 st.title("환영합니다🤗")
-st.markdown("@author: [youngmo](https:github.com/youngmo123)")
-st.markdown("[이메일](mailto:youngmo123@hanmail.net)")
 
+st.markdown("@author: [배영모](https:github.com/youngmo123)")
+st.markdown("[이메일](mailto:youngmo123@hanmail.net)")
 
 st.markdown(
     """
@@ -13,13 +13,32 @@ OpenAI API 키 발급 방법은 아래 링크를 참고해 주세요!
 """
 )
 
+# API 키 입력
+api_key = st.text_input("OPENAI API 키 입력(ChatGPT)", type="password")
+
+
+st.markdown(
+    """
+Tavily API 키 발급 방법은 아래 링크를 참고해 주세요! (검색용)
+* [발급방법](https://wikidocs.net/233342)
+"""
+)
 
 # API 키 입력
-api_key = st.text_input("API 키 입력", type="password")
+search_api_key = st.text_input("Tavily Search API 키 입력(검색용)", type="password")
+
 
 # 설정 확인 버튼
 confirm_btn = st.button("설정하기", key="api_key")
 
+
 if confirm_btn:
-    os.environ["OPENAI_API_KEY"] = api_key
-    st.write(f"API 키가 설정되었습니다: `{api_key[:15]}************`")
+    if api_key:
+        os.environ["OPENAI_API_KEY"] = api_key
+        st.write(f"OPENAI API 키가 설정되었습니다: `{api_key[:15]}************`")
+
+    if search_api_key:
+        os.environ["TAVILY_API_KEY"] = search_api_key
+        st.write(
+            f"TAVILPY API 키가 설정되었습니다: `{search_api_key[:15]}************`"
+        )
